7f28d92fc3d198cac9fdde6fe0c57a8bf23dae5f
zq12138 <1453759654@qq.com>
2016-10-31 09:49:42 +0800

    测试信息

--- app/src/main/java/com/example/admin/testandroidstudioupgithub2/MainActivity.java
+++ app/src/main/java/com/example/admin/testandroidstudioupgithub2/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.admin.testandroidstudioupgithub2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 这是一个测试文件
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
